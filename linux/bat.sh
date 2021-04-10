e2e7d371d9b99a3d30dd7419212457615ed15235
Takumi Kanada <candy12t@gmail.com>
2021-04-10 22:54:01 +0900

    add: install shell

---
+++ linux/bat.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# https://github.com/sharkdp/bat
+
+curl -LO https://github.com/sharkdp/bat/releases/download/v0.18.0/bat_0.18.0_amd64.deb
+sudo dpkg -i bat_0.18.0_amd64.deb
---
+++ linux/ripgrep.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# https://github.com/BurntSushi/ripgrep
+
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb
+sudo dpkg -i ripgrep_12.1.1_amd64.deb
