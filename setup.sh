1ed92933873882a71f55eb22bf14648890e6ac48
Takumi Kanada <candy12t@gmail.com>
2021-06-07 00:09:06 +0900

    update: setup.sh

--- setup.sh
+++ setup.sh
@@ -3,16 +3,24 @@
 set -eu
 
 script_home="$(cd $(dirname $0); pwd)"
-cd ${script_home}
+cd "${script_home}"
 
-for f in .??*
-do
-  [[ "$f" == ".git" ]] && continue
-  [[ "$f" == ".DS_Store" ]] && continue
+config=".config"
+config_files=("${config}/nvim" "${config}/starship.toml" "${config}/git")
+dotfiles=(".gitconfig" ".tmux.conf" ".vimrc" ".zpreztorc" ".zshrc")
 
-  if [ -e "$HOME/$f" ]; then
-    unlink $HOME/$f
-  fi
-  ln -s $(cd $(dirname $0); pwd)/$f $HOME/$f
-  echo "created symbolic link: $f"
-done
+function create_symbolic_link () {
+  for f in `echo "$*"`; do
+    if [ -e "${HOME}/${f}" ]; then
+      unlink "${HOME}/${f}"
+    fi
+    ln -s "$(cd $(dirname $0); pwd)/${f}" "${HOME}/${f}"
+    echo "created symbolic link: ${f}"
+  done
+}
+
+if [ ! -d ${config} ]; then
+  mkdir ${config}
+fi
+create_symbolic_link "${config_files[@]}"
+create_symbolic_link "${dotfiles[@]}"
