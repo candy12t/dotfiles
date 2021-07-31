0c3d14f0c10d9d5731a7e1a4c14c4752c2807309
Takumi Kanada <candy12t@gmail.com>
2021-07-31 10:49:29 +0900

    Update: brew

--- mac/brew/backup_brew.sh
+++ mac/brew/backup_brew.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
+set -eu
+
+script_home="$(cd $(dirname $0); pwd)"
+cd "${script_home}"
+
 rm .brewfile
 brew bundle dump --file=.brewfile
+
+exit 0;
--- mac/brew/setup_brew.sh
+++ mac/brew/setup_brew.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
+set -eu
+
+script_home="$(cd $(dirname $0); pwd)"
+cd "${script_home}"
+
 brew bundle install --file=.brewfile
+
+exit 0;
