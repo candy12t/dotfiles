3fcad160f154a0999ddcffbd9f961a2e4c383df2
Takumi Kanada <candy12t@gmail.com>
2021-10-30 05:08:22 +0000

    Remove installer of tool

--- linux/starship.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-# https://github.com/starship/starship
-
-curl -fsSL https://starship.rs/install.sh | bash
--- linux/zprezto.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-# https://github.com/sorin-ionescu/prezto
-
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
