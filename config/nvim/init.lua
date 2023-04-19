5441b20da689454e17e216a1a7b91ae5f104cf9e
Takumi Kanada <candy12t@gmail.com>
2023-04-19 20:15:17 +0900

    Update: nvim setting

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,27 @@
+vim.opt.fencs = "utf-8"
+vim.opt.encoding = "utf-8"
+
+vim.opt.autoread = true
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.hidden = true
+
+vim.opt.clipboard:append("unnamedplus")
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 vim.opt.number = true
 vim.opt.cursorline = true
+
+vim.opt.termguicolors = true
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
