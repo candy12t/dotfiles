c96819e183a58ff27e7feacf90c3b7777d0486a0
Takumi Kanada <candy12t@gmail.com>
2023-08-20 19:58:38 +0900

    Update: install script

--- install.sh
+++ install.sh
@@ -1,133 +1,50 @@
 #! /bin/bash
 
 set -eu
+set -o pipefail
 
-DOTFILES_PATH="${HOME}/dotfiles"
-DOTFILES_REPO="https://github.com/candy12t/dotfiles.git"
-DOTFILES_BRANCH="master"
+readonly DOTFILES_PATH="${HOME}/dotfiles"
+readonly DOTFILES_REPO="https://github.com/candy12t/dotfiles.git"
+readonly DOTFILES_BRANCH="master"
 
-arch=$(uname -sm)
-
-exe_time=$(date '+%Y%m%d%H%M%S')
-
-dotfiles::download_or_update_repository() {
+download_or_update_repository() {
   if [ -d "${DOTFILES_PATH}" ]; then
     echo "Updating dotfiles..."
-    dotfiles::update_repository
+    update_repository
   elif [ ! -d "${DOTFILES_PATH}" ]; then
     echo "Downloading dotfiles..."
-    dotfiles::download_repository
+    download_repository
   fi
 }
 
-dotfiles::update_repository() {
+update_repository() {
   git --git-dir="${DOTFILES_PATH}/.git" pull origin master
 }
 
-dotfiles::download_repository() {
+download_repository() {
   git clone "${DOTFILES_REPO}" --branch "${DOTFILES_BRANCH}" "${DOTFILES_PATH}"
 }
 
-dotfiles::setting_macos() {
-  echo "Setting os..."
-  dotfiles::install_homebrew
+link_rc() {
+  echo "link rc..."
+
+  mkdir -p "${HOME}/.config"
+  mkdir -p "${HOME}/.config/git"
+
+  ln -sf "${DOTFILES_PATH}/config/nvim" "${HOME}/.config/nvim"
+  ln -sf "${DOTFILES_PATH}/config/gitignore" "${HOME}/.config/git/ignore"
+  ln -sf "${DOTFILES_PATH}/config/starship.toml" "${HOME}/.config/starship.toml"
+
+  ln -sf "${DOTFILES_PATH}/config/gitconfig" "${HOME}/.gitconfig"
+  ln -sf "${DOTFILES_PATH}/config/tmux.conf" "${HOME}/.tmux.conf"
+  ln -sf "${DOTFILES_PATH}/config/zpreztorc" "${HOME}/.zpreztorc"
+  ln -sf "${DOTFILES_PATH}/config/zshrc" "${HOME}/.zshrc"
 }
 
-dotfiles::install_homebrew() {
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+main() {
+  download_or_update_repository
+  link_rc
+  echo "success!"
 }
 
-dotfiles::install_langs() {
-  echo "Installing languages..."
-  for f in $(/bin/ls "${DOTFILES_PATH}/installer/langs"); do
-    echo "Installing ${f}..."
-    "${DOTFILES_PATH}/installer/langs/${f}/install.sh"
-  done
-}
-
-dotfiles::install_cli_tools() {
-  echo "Installing CLI tools..."
-  for f in $(/bin/ls "${DOTFILES_PATH}/installer/tools"); do
-    echo "Installing ${f}..."
-    "${DOTFILES_PATH}/installer/tools/${f}/install.sh"
-  done
-}
-
-dotfiles::setting_rc() {
-  echo "Setting rc..."
-  for f in $(/bin/ls "${DOTFILES_PATH}/config"); do
-    if [ -d "${DOTFILES_PATH}/config/${f}" ]; then
-      mkdir -p "$HOME/.${f}"
-      for files in $(/bin/ls "${DOTFILES_PATH}/config/${f}"); do
-        dotfiles::backup_config "${f}/${files}"
-        dotfiles::setting_config "${f}/${files}"
-      done
-      continue
-    fi
-    dotfiles::backup_config "${f}"
-    dotfiles::setting_config "${f}"
-  done
-}
-
-dotfiles::backup_config() {
-  local f="$1"
-  local dotfile=".$1"
-  if [ -e "$HOME/${dotfile}" ]; then
-    mv "$HOME/${dotfile}" "$HOME/${dotfile}.${exe_time}_backup" && echo "backuped $HOME/${dotfile}"
-  fi
-}
-
-dotfiles::setting_config() {
-  local f="$1"
-  ln -s "${DOTFILES_PATH}/config/${f}" "${HOME}/.${f}" && echo "created symbolic link: ${f}"
-}
-
-dotfiles::setting_shell() {
-  echo "Setting shell..."
-  [[ $(echo "$SHELL") =~ "zsh" ]] && return
-
-  if [ $(which zsh) >/dev/null 2>&1 ]; then
-    dotfiles::install_zsh
-  fi
-
-  chsh -s $(which zsh)
-  exec $(which zsh) -l
-}
-
-dotfiles::install_zsh() {
-  echo "Installing zsh..."
-  "${DOTFILES_PATH}/installer/tools/zsh/install.sh"
-}
-
-dotfiles::install_for_mac() {
-  dotfiles::download_or_update_repository
-  dotfiles::setting_macos
-  dotfiles::install_langs
-  dotfiles::install_cli_tools
-  dotfiles::setting_rc
-  dotfiles::setting_shell
-}
-
-dotfiles::install_for_linux() {
-  dotfiles::download_or_update_repository
-  dotfiles::install_langs
-  dotfiles::install_cli_tools
-  dotfiles::setting_rc
-  dotfiles::setting_shell
-}
-
-dotfiles::main() {
-  case "${arch}" in
-    Darwin\ *64)
-      dotfiles::install_for_mac
-      ;;
-    Linux\ *64)
-      dotfiles::install_for_linux
-      ;;
-    *)
-      echo "Sorry! Not supported architectures."
-      ;;
-  esac
-}
-
-dotfiles::main && exit 0;
+main "$@" && exit 0;
