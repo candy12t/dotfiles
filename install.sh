dc83849a75a490ec23cff13ae1f2f056dac5025f
Takumi Kanada <candy12t@gmail.com>
2024-01-06 15:24:33 +0900

    Add: memo config

--- install.sh
+++ install.sh
@@ -3,34 +3,50 @@
 set -eu
 set -o pipefail
 
-readonly DOTFILES_PATH="${HOME}/dotfiles"
+readonly DOTFILES_PATH="$HOME/dotfiles"
 readonly DOTFILES_REPO="https://github.com/candy12t/dotfiles.git"
 readonly DOTFILES_BRANCH="master"
 
 download_repository() {
   echo "Downloading dotfiles..."
-  git clone "${DOTFILES_REPO}" --branch "${DOTFILES_BRANCH}" "${DOTFILES_PATH}"
+  git clone "$DOTFILES_REPO" --branch "$DOTFILES_BRANCH" "$DOTFILES_PATH"
 }
 
 link_rc() {
   echo "link rc..."
 
-  mkdir -p "${HOME}/.config"
-  mkdir -p "${HOME}/.config/git"
-  mkdir -p "${HOME}/.config/tmux"
+  mkdir -p "$HOME/.config" \
+    "$HOME/.config/git" \
+    "$HOME/.config/tmux" \
+    "$HOME/.config/memo" \
+    "$HOME/.config/memo/plugins" \
+    "$HOME/.config/memo/templates"
 
-  ln -sf "${DOTFILES_PATH}/config/nvim" "${HOME}/.config/nvim"
+  # nvim
+  ln -sfn "$DOTFILES_PATH/config/nvim" "$HOME/.config/nvim"
 
-  ln -sf "${DOTFILES_PATH}/config/gitignore" "${HOME}/.config/git/ignore"
-  ln -sf "${DOTFILES_PATH}/config/gitconfig" "${HOME}/.config/git/config"
-  ln -sf "${DOTFILES_PATH}/config/tmux.conf" "${HOME}/.config/tmux/tmux.conf"
-  ln -sf "${DOTFILES_PATH}/config/starship.toml" "${HOME}/.config/starship.toml"
+  # git
+  ln -sfn "$DOTFILES_PATH/config/gitignore" "$HOME/.config/git/ignore"
+  ln -sfn "$DOTFILES_PATH/config/gitconfig" "$HOME/.config/git/config"
 
-  ln -sf "${DOTFILES_PATH}/config/zpreztorc" "${HOME}/.zpreztorc"
-  ln -sf "${DOTFILES_PATH}/config/zshrc" "${HOME}/.zshrc"
+  # tmux
+  ln -sfn "$DOTFILES_PATH/config/tmux.conf" "$HOME/.config/tmux/tmux.conf"
+
+  # memo(https://github.com/mattn/memo), mamolist.vim(https://github.com/glidenote/memolist.vim)
+  ln -sfn "$DOTFILES_PATH/config/memo/config.toml"      "$HOME/.config/memo/config.toml"
+  ln -sfn "$DOTFILES_PATH/config/memo/templates/md.txt" "$HOME/.config/memo/templates/md.txt"
+  ln -sfn "$DOTFILES_PATH/config/memo/plugins/commit"   "$HOME/.config/memo/plugins/commit"
+
+  # starship
+  ln -sfn "$DOTFILES_PATH/config/starship.toml" "$HOME/.config/starship.toml"
+
+  # zsh
+  ln -sfn "$DOTFILES_PATH/config/zpreztorc" "$HOME/.zpreztorc"
+  ln -sfn "$DOTFILES_PATH/config/zshrc"     "$HOME/.zshrc"
 
   if [ $(uname -s) = "Darwin" ]; then
-    ln -sf "${DOTFILES_PATH}/config/hammerspoon" "${HOME}/.hammerspoon"
+    # hammerspoon
+    ln -sfn "$DOTFILES_PATH/config/hammerspoon" "$HOME/.hammerspoon"
   fi
 }
 
