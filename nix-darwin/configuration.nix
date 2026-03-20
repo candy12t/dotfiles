7183de40883706f5770b149a0904eab2f7eb867a
Takumi Kanada <candy12t@gmail.com>
2026-03-20 16:44:07 +0900

    feat: record the build revision.

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -1,7 +1,8 @@
-{ ... }:
+{ self, ... }:
 {
   nixpkgs.hostPlatform = "aarch64-darwin";
   system.stateVersion = 6;
   nix.enable = false;
   security.pam.services.sudo_local.touchIdAuth = true;
+  system.configurationRevision = self.rev or self.dirtyRev or null;
 }
