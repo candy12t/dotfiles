7fd80bc0df433872a38fe79248db25568d5d7577
Takumi Kanada <candy12t@gmail.com>
2026-03-20 15:58:05 +0900

    sudo nix run nix-darwin/master#darwin-rebuild -- switch --flake .

--- flake.nix
+++ flake.nix
@@ -1,17 +1,24 @@
 {
-  description = "Home Manager configuration of candy12t";
+  description = "My dotfiles";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
+    { nixpkgs, home-manager, nix-darwin, ... }:
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -19,13 +26,10 @@
     {
       homeConfigurations."candy12t" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
         modules = [ ./home-manager/home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+      };
+      darwinConfigurations."MacBookAir" = nix-darwin.lib.darwinSystem {
+        modules = [ ./nix-darwin/configuration.nix ];
       };
     };
 }
