515abf3084383469b823487fb7a2adc98756c92a
Takumi Kanada <candy12t@gmail.com>
2026-03-20 19:56:54 +0900

    feat: darwin minimum setting

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -1,11 +1,24 @@
 { self, ... }:
 {
-  nixpkgs.hostPlatform = "aarch64-darwin";
-  system.stateVersion = 6;
+  nixpkgs = {
+    hostPlatform = "aarch64-darwin";
+  };
+
+  system = {
+    stateVersion = 6;
+    configurationRevision = self.rev or self.dirtyRev or null;
+    primaryUser = "candy12t";
+  };
+
+  users.users.candy12t = {
+    home = "/Users/candy12t";
+    name = "candy12t";
+  };
+
   nix.enable = false;
+
   security.pam.services.sudo_local = {
     reattach = true;
     touchIdAuth = true;
   };
-  system.configurationRevision = self.rev or self.dirtyRev or null;
 }
