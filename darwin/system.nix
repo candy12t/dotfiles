fa3f6a16daa41c1630318074980658d243890ec3
Takumi Kanada <candy12t@gmail.com>
2026-04-10 15:17:11 +0900

    feat: darwin system setting with nix.

--- darwin/system.nix
+++ darwin/system.nix
@@ -3,6 +3,61 @@
   system = {
     stateVersion = 6;
     configurationRevision = inputs.rev or inputs.dirtyRev or null;
+
+    defaults = {
+      NSGlobalDomain = {
+        AppleEnableMouseSwipeNavigateWithScrolls = true;
+        AppleEnableSwipeNavigateWithScrolls = true;
+        AppleInterfaceStyle = "Dark";
+        KeyRepeat = 2;
+      };
+
+      dock = {
+        autohide = true;
+        autohide-delay = 0.0;
+        launchanim = false;
+        mru-spaces = false;
+        orientation = "bottom";
+        show-process-indicators = true;
+        show-recents = false;
+        tilesize = 50;
+      };
+
+      finder = {
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        FXDefaultSearchScope = "SCcf";
+        FXEnableExtensionChangeWarning = true;
+        ShowPathbar = true;
+        ShowStatusBar = true;
+        _FXSortFoldersFirst = true;
+      };
+
+      screencapture = {
+        disable-shadow = true;
+      };
+
+      trackpad = {
+        ActuateDetents = true;
+        Clicking = false;
+        FirstClickThreshold = 1;
+        SecondClickThreshold = 1;
+        TrackpadPinch = true;
+        TrackpadRightClick = true;
+        TrackpadThreeFingerHorizSwipeGesture = 2;
+        TrackpadThreeFingerVertSwipeGesture = 2;
+        TrackpadTwoFingerFromRightEdgeSwipeGesture = 3;
+      };
+    };
+
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToControl = true;
+    };
+  };
+
+  time = {
+    timeZone = "Asia/Tokyo";
   };
 
   nix.enable = false;
