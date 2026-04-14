032f43040a1c42e5522792f27fd5c88e38ed21e2
Takumi Kanada <candy12t@gmail.com>
2026-04-14 23:41:32 +0900

    feat: nix enable

--- darwin/system.nix
+++ darwin/system.nix
@@ -60,7 +60,27 @@
     timeZone = "Asia/Tokyo";
   };
 
-  nix.enable = false;
+  nix = {
+    enable = true;
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      keep-build-log = true;
+      keep-derivations = false;
+      keep-outputs = false;
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+  };
 
   security.pam.services.sudo_local = {
     reattach = true;
